855469c28252836883c20012a10bafb969098096
Fjolla2002 <fjolla.selimi17@gmail.com>
2023-11-04 12:04:19 +0100

    Added our specializations section of homepage

--- edma-fs/src/pages/HomePage.jsx
+++ edma-fs/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../components/HomeComponents/About/About'
+import Specialization from '../components/HomeComponents/Specialization/Specialization'
 
 const HomePage = () => {
   return (
     <div>
       <About />
+      <Specialization/>
     </div>
   )
 }
